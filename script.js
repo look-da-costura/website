317d839057fbd54cd1e05e86260cd0ab027be43f
William Araujo <williamf1f2@gmail.com>
2022-04-03 22:57:53 -0300

    [section-title] new: add style for the title of each section in main content

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const cards = document.querySelectorAll('.card');
+const sectionTitleEffects = document.querySelectorAll('.title-effect');
+const sectionTitleTexts = document.querySelectorAll('.section__title');
 
-cards.forEach(element => {
-  element.style.backgroundColor = element.dataset.cardBackgroundColor;
+cards.forEach(card => {
+  card.style.backgroundColor = card.dataset.cardBackgroundColor;
 
-  element.style.boxShadow = `-10px 10px ${element.dataset.cardShadowColor}`;
+  card.style.boxShadow = `-10px 10px ${card.dataset.cardShadowColor}`;
 });
+
+sectionTitleEffects.forEach(titleEffect => {
+  titleEffect.style.backgroundColor = titleEffect.dataset.titleBackgroundColor;
+});
+
+sectionTitleTexts.forEach(titleText => {
+  titleText.style.color = titleText.dataset.titleColor;
+})
